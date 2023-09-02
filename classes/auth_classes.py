84a61e0cfc19a729486825a89cd98beac4a0bf43
Ernestas Araminas <51124312+ernara@users.noreply.github.com>
2023-09-02 05:30:24 +0300

    added register option

---
+++ classes/auth_classes.py
@@ -0,0 +1,15 @@
+from wtforms import StringField, PasswordField, SubmitField
+from flask_wtf import FlaskForm
+from wtforms.validators import InputRequired, Length, Email, EqualTo
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[InputRequired(), Length(min=4, max=20)])
+    password = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=80)])
+    submit = SubmitField('Login')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[InputRequired(), Length(min=4, max=20)])
+    email = StringField('Email', validators=[InputRequired(), Email()])
+    password = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=80)])
+    confirm_password = PasswordField('Confirm Password', validators=[InputRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
--- routers/auth_router.py
+++ routers/auth_router.py
@@ -2,8 +2,9 @@ from flask import Blueprint, session,flash, render_template, redirect, url_for, 
 from models.note_model import Note
 from db import db
 from models.user_model import User
-from  classes.loginForm_class import LoginForm
+from classes.auth_classes import LoginForm
 from werkzeug.security import generate_password_hash, check_password_hash
+from classes.auth_classes import RegistrationForm
 
 auth_router = Blueprint('auth_router', __name__)
 
@@ -37,3 +38,25 @@ def dashboard():
 def logout():
     session.pop('user_id', None)
     return redirect(url_for('login'))
+
+
+@auth_router.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+
+    if form.validate_on_submit():
+        username = form.username.data
+        email = form.email.data
+        password = form.password.data
+
+        if User.query.filter_by(username=username).first() or User.query.filter_by(email=email).first():
+            flash('Username or email already in use', 'error')
+        else:
+            hashed_password = generate_password_hash(password, method='sha256')
+            new_user = User(username=username, email=email, password=hashed_password)
+            db.session.add(new_user)
+            db.session.commit()
+            flash('Registration successful!', 'success')
+            return redirect(url_for('login'))
+
+    return render_template('register.html', form=form)
