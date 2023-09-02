78307683a1c3ed656da5e2d33abbb53f80dc5361
Ernestas Araminas <51124312+ernara@users.noreply.github.com>
2023-09-02 06:46:09 +0300

    small fixes

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from flask import render_template
 from db import db
-from flasky import app
+from flask import Flask
+from routers.note_router import note_router
+from routers.auth_router import auth_router
+from config import Config
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'
-app.config['SECRET_KEY'] = 'your_secret_key'  
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app = Flask(__name__)
+app.config.from_object(Config)
 
 db.init_app(app)
+
+app.register_blueprint(note_router)
+app.register_blueprint(auth_router)
 
 @app.route('/')
 def index():
