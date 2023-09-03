35df9f9cba54bec7ce676224b8ddc43bf4162793
Ernestas Araminas <51124312+ernara@users.noreply.github.com>
2023-09-03 16:02:51 +0300

    added function: create note by pressing enter

--- static/scripts/note_view.js
+++ static/scripts/note_view.js
@@ -2,9 +2,9 @@ const noteTitleInput = $('#note-title');
 const noteTextInput = $('#note-text');
 const noteArea = $('#notes-container');
 
-import {createNoteOnServer, deleteNoteOnServer, GetNotesFromServer, deleteAllNotesOnServer } from "./note_api.js";
+import { createNoteOnServer, deleteNoteOnServer, GetNotesFromServer, deleteAllNotesOnServer } from "./note_api.js";
 
-$(document).ready(function() {
+$(document).ready(function () {
 
     const createNoteButton = $('#create-note');
     createNoteButton.on('click', createNoteOnSite);
@@ -13,8 +13,21 @@ $(document).ready(function() {
     const sortNotesButton = $('#sort-notes');
     sortNotesButton.on('click', loadNotesOnSite);
 
-    async function createNoteOnSite() {
+    noteTitleInput.on('keydown', function (e) {
+        if (e.key === 'Enter') {
+            e.preventDefault(); 
+            noteTextInput.focus(); 
+        }
+    });
 
+    noteTextInput.on('keydown', async function (e) {
+        if (e.key === 'Enter') {
+            e.preventDefault(); 
+            await createNoteOnSite(); 
+        }
+    });
+
+    async function createNoteOnSite() {
         const data = await createNoteOnServer();
         const newNote = $('<div>').addClass('note note' + data.id);
         const title = stripHtml(data.title);
@@ -22,27 +35,24 @@ $(document).ready(function() {
         const html = `<h3>${title}</h3><p>${text}</p><span class="delete-note" data-id="${data.id}">&times;</span>`;
         newNote.html(html);
         newNote.appendTo(noteArea);
-        newNote.draggable(); 
-        
+        newNote.draggable();
+
         applyDeleteListener(newNote);
         clearNoteForm();
     }
 
     async function loadNotesOnSite() {
-        
         noteArea.empty();
         const data = await GetNotesFromServer();
-            
+
         if (Array.isArray(data)) {
             const noteArea = $('#notes-container');
 
-            let i =0;
-            let inOneCell = Math.round(Math.sqrt(data.length))+1;
-            let boundaries = 1.3
-
+            let i = 0;
+            let inOneCell = Math.round(Math.sqrt(data.length)) + 1;
+            let boundaries = 1.3;
 
             data.forEach(note => {
-
                 const newNote = $('<div>').addClass('note note' + note.id);
                 const title = stripHtml(note.title);
                 const text = stripHtml(note.text).replace(/\r\n|\r|\n/g, '<br />');
@@ -50,9 +60,9 @@ $(document).ready(function() {
                 newNote.html(html);
                 newNote.appendTo(noteArea);
 
-                const top =  Math.floor(i / inOneCell) * newNote.height() * boundaries;
+                const top = Math.floor(i / inOneCell) * newNote.height() * boundaries;
                 const left = (i % inOneCell) * newNote.width() * boundaries;
-            
+
                 newNote.css({
                     'position': 'absolute',
                     'top': top + 'px',
@@ -66,11 +76,10 @@ $(document).ready(function() {
         }
     }
 
-    
     function applyDeleteListener(noteElement) {
-        $('.delete-note', noteElement).click(function() {
+        $('.delete-note', noteElement).click(function () {
             const noteId = $(this).data('id');
-            $(this).closest('.note' + noteId).remove(); 
+            $(this).closest('.note' + noteId).remove();
             deleteNoteOnServer(noteId);
         });
     }
@@ -83,11 +92,10 @@ $(document).ready(function() {
         }
     }
 
-        
     function stripHtml(text) {
         return text.replace(/<\/?[^>]+(>|$)/g, '');
     }
-    
+
     function clearNoteForm() {
         noteTitleInput.val('');
         noteTextInput.val('');
