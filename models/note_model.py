9cdc0c4cf0a73e4f1bf2efe4849dec753f3c6c67
Ernestas Araminas <51124312+ernara@users.noreply.github.com>
2023-10-05 12:26:07 +0300

    FIxed a bug where users were not working

--- models/note_model.py
+++ models/note_model.py
@@ -1,17 +1,17 @@
 from db import db
+from .user_model import User
 
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
     text = db.Column(db.Text)
 
-    # user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=True)
 
-    # def __init__(self, title, text, user_id):
-    def __init__(self, title, text):
+    def __init__(self, title, text, user_id=None):
         self.title = title
         self.text = text
-        # self.user_id = user_id
+        self.user_id = user_id
 
     def __repr__(self):
         return f"<Note {self.title}>"
@@ -21,5 +21,5 @@ class Note(db.Model):
             'id': self.id,
             'title': self.title,
             'text': self.text,
-            # 'user_id': self.user_id
+            'user_id': self.user_id
         }
--- models/user_model.py
+++ models/user_model.py
@@ -1,28 +1,17 @@
 from db import db
 
 class User(db.Model):
+    __tablename__ = 'user' 
+
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100), unique=True, nullable=False)
-    email = db.Column(db.String(100), unique=True, nullable=False)
-    password = db.Column(db.String(100), nullable=False)
     oauth2_token = db.Column(db.String(100))
 
     notes = db.relationship('Note', backref='user', lazy=True)
 
-    def __init__(self, username, email, password, oauth2_token=None):
-        self.username = username
-        self.email = email
-        self.password = password
+    def __init__(self, oauth2_token=None):
         self.oauth2_token = oauth2_token
 
-    def __repr__(self):
-        return f"<User {self.username}>"
-    
     def serialize(self):
         return {
-            'id': self.id,
-            'username': self.username,
-            'email': self.email,
             'oauth2_token': self.oauth2_token
         }
-
